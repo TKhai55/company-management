2ea263f74dc9cb77fe42b45150e4838e9415858a
TKhai55 <thienkhaiak5555@gmail.com>
2023-05-06 05:01:16 +0700

    beginning

---
+++ src/Views/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Menu} from 'antd';
+import type { MenuProps } from 'antd';
+import './SideMenu.css'
+import { useNavigate } from 'react-router-dom'
+import { UserOutlined, SettingOutlined } from '@ant-design/icons';
+
+let myArray = [
+  { label: 'test 10', key: 'test1', icon: 'SettingOutlined' },
+  { label: 'test 2', key: 'test2', icon: 'SettingOutlined' },
+  { label: 'test 3', key: 'test3', icon: 'SettingOutlined' },
+  // ...
+];
+
+const menuItems = myArray.map((obj) => (
+  <Menu.Item key={obj.key} icon={React.createElement(obj.icon)}>
+    {obj.label}
+  </Menu.Item>
+));
+
+
+const SideMenu = () => {
+    const navigate = useNavigate()
+  return (
+        <Menu
+        mode='inline'
+        className='SideMenu-container'
+        onClick={({key})=>{
+          navigate(key)
+        }}>
+          {menuItems}
+        </Menu>
+   
+  )
+}
+
+export default SideMenu
