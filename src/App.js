0255287d4c7b06d2169077c47cf49ecb74172076
TKhai55 <thienkhaiak5555@gmail.com>
2023-05-08 16:35:38 +0700

    semi -fix icon sidebar

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Views/Login/Login";
 import JitsiMeet from "./Views/JitsiMeet";
 import Header from "./Views/components/Header/Header";
-import SideMenu from "./Views/components/SideMenu/SideMenu.tsx";
+import SideMenu from "./Views/components/SideMenu/SideMenu";
 import { Layout } from "antd";
 import Test from "./Views/components/Test";
 
 
+
+const islogin = true;
+
 function App() {
+  if(islogin === false) return(
+    <Routes>
+      <Route path="/signin" element={<Login/>} />
+    </Routes>
+  ) 
+  else 
   return (
+    
     <div className="App-container">
       <Header/>
       <div className="App-Content-container">
         <SideMenu/>
         <div className="App-Content-Main">
-
           <Content/>
         </div>
       </div>
-
     </div>
-    // <Layout>
-    //   <Layout.Sider >
-    //     <SideMenu/>
-    //   </Layout.Sider>
-    //   <Layout.Content>
-    //     <Content/>
-    //   </Layout.Content>
-    // </Layout>
   );
 }
 
@@ -36,13 +36,11 @@ function Content(){
   return(
     
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/test1" element={<div style={{height: '200%'}}>test1</div>} />
+        <Route path="/" element={<div style={{height: '200%'}}>test1</div>} />
         <Route path="/test2" element={<Test/>} />
         <Route path="/test3" element={<div>test3</div>} />
         <Route path="/test4" element={<div>test4</div>} />
         <Route path="/test5" element={<div>test5</div>} />
-        {/* <Route path="/video" element={<JitsiMeet />}></Route> */}
       </Routes>
     
   )
