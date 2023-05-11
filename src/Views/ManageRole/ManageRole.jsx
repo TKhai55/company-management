e97a584a7dec26721adab84783a546fd58802dea
TKhai55 <thienkhaiak5555@gmail.com>
2023-05-11 13:53:18 +0700

    fix

--- src/Views/ManageRole/ManageRole.jsx
+++ src/Views/ManageRole/ManageRole.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from '../components/Header/Header'
+import SideMenu from '../components/SideMenu/SideMenu'
 
 function ManageRole() {
   return (
-    <div>
-        ManageRole
+    <div className="App-container">
+      <Header/>
+      <div className="App-Content-container">
+        <SideMenu/>
+        <div className="App-Content-Main">
+        <div style={{height: "200%"}}>
+            Manage Role
+         </div>
+        </div>
+      </div>
     </div>
   )
 }
